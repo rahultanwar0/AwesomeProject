c5012d68a63f4811ccce481eb94180ca987f352e
RAHUL TANWAR <rahul@magicapps.co.in>
2021-12-07 12:31:34 +0530

    map an array

--- App.js
+++ App.js
@@ -14,11 +14,16 @@ import {
  
 export default function App() {
   let name = "hello world"
+  let array = [{id:"1",name:"rahul"},{id:"2",name:"deepak"},{id:"3",name:"sachin"}]
   return (
     <View>
       {
         // name ? <Text>{name}</Text> : <Text>no name</Text>
-        name && <Text>{name}</Text>
+        // name && <Text>{name}</Text>
+        // array.map(item=> <Text>{item.name}</Text>)
+        array.map(item=> {
+          return <Text>{item.name}</Text>
+        })
 
       }
      </View>
