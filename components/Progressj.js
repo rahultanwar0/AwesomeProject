2e2b83fd8fc9decb5f749e9eb97fdc0fd30980c7
RAHUL TANWAR <rahul@magicapps.co.in>
2021-12-23 16:06:02 +0530

    progresssj

---
+++ components/Progressj.js
@@ -0,0 +1,20 @@
+import React, {Profiler, useState} from 'react';
+import {
+  ToastAndroid,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Alert,
+  ActivityIndicator,
+  ScrollViewComponent,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  Modal,
+} from 'react-native';
+
+export function Progresss() {
+  return <ActivityIndicator size={100} color="primary" />;
+}
