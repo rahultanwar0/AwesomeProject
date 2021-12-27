5ec2ab640aec8d57ea5f83c519d59e5f65d17993
RAHUL TANWAR <rahul@magicapps.co.in>
2021-12-27 11:39:53 +0530

    oncptl

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {Profiler, useState,useEffect} from 'react';
+import React, {Profiler, useState, useEffect} from 'react';
 import {
   ToastAndroid,
   Text,
@@ -22,103 +22,91 @@ import {MyPdfImage} from './components/MyImageViewContainer';
 import {MyJokes} from './components/JokeFromInternet';
 import {MyBox} from './components/Mybox';
 import {Progresss} from './components/Progressj';
- import Login from './components/Login';
-import { RecyclerListView } from 'recyclerlistview';
-
+import Login from './components/Login';
+ 
 export default function App() {
   const [myname, setmyname] = useState('tanwar');
   const [loading, setloading] = useState(false);
   const [modalw, setModal] = useState(false);
 
-
-  
-
   return (
     <View style={style.container}>
-
-
       <ScrollView>
+        <MyJokes />
 
+        <Progresss />
+        <ShowListInTextView />
+        <Login />
+        <MyAlertBox />
+        <Touchhablecomponent />
+        <MyBox />
 
-      <MyJokes />
+        {/* <render /> */}
 
-<Progresss />
-<ShowListInTextView />
-<Login />
-<MyAlertBox />
-<Touchhablecomponent />
-<MyBox />
+        <Image
+          source={require('./assets/plash.png')}
+          style={{hight: 200, width: 300}}
+        />
 
+        {
+          // name ? <Text>{name}</Text> : <Text>no name</Text>
+          // name && <Text>{name}</Text>
+          // array.map(item=> <Text>{item.name}</Text>)
+          // array.map(item=> {
+          //   return <Text>{item.name}</Text>
+          // })
 
+          myname ? <Text>{myname}</Text> : <Text>No name</Text>
+        }
 
-<Image
-        source={require('./assets/plash.png')}
-        style={{hight: 200, width: 300}}
-      />
-
-      {
-        // name ? <Text>{name}</Text> : <Text>no name</Text>
-        // name && <Text>{name}</Text>
-        // array.map(item=> <Text>{item.name}</Text>)
-        // array.map(item=> {
-        //   return <Text>{item.name}</Text>
-        // })
-
-        myname ? <Text>{myname}</Text> : <Text>No name</Text>
-      }
-
-      <Button
-        title="Button1"
-        color="black"
-        onPress={() => {
-          setmyname('Button1');
-        }}
-      />
-      <Button
-        title="Button2"
-        color="black"
-        onPress={() => {
-          setmyname('Button2');
-        }}
-      />
-
-      <TextInput
-        placeholder="Type here"
-        style={{hight: 50, borderWidth: 2}}
-        onChangeText={text => {
-          console.log(text);
-          setmyname(text);
-        }}
-      />
-
-      <Modal
-        visible={modalw}
-        onRequestClose={() => {
-          setModal(false);
-        }}
-        animationType="slide">
-        <Text>hello sir</Text>
         <Button
-          title="Back To Main Modal"
+          title="Button1"
+          color="black"
           onPress={() => {
-            setModal(false);
+            setmyname('Button1');
+          }}
+        />
+        <Button
+          title="Button2"
+          color="black"
+          onPress={() => {
+            setmyname('Button2');
           }}
         />
 
+        <TextInput
+          placeholder="Type here"
+          style={{hight: 50, borderWidth: 2}}
+          onChangeText={text => {
+            console.log(text);
+            setmyname(text);
+          }}
+        />
 
-        <MyJokes/>
-      </Modal>
+        <Modal
+          visible={modalw}
+          onRequestClose={() => {
+            setModal(false);
+          }}
+          animationType="slide">
+          <Text>hello sir</Text>
+          <Button
+            title="Back To Main Modal"
+            onPress={() => {
+              setModal(false);
+            }}
+          />
 
-      <Button
-        title="New Modal"
-        onPress={() => {
-          setModal(true);
-        }}
-      />
+          <MyJokes />
+        </Modal>
 
+        <Button
+          title="New Modal"
+          onPress={() => {
+            setModal(true);
+          }}
+        />
       </ScrollView>
-     
-     
     </View>
   );
 }
